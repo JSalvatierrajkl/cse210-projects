9df608e01e1a8c088772a5762a3973dfcd8670a7
Joel Salvatierra <joeljsalvatierra@gmail.com>
2023-04-17 23:27:46 -0300

    W02 Prove: Developer—C# Programming Parts 1-2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter your grade percentage: ");
+        string userInput = Console.ReadLine();
+        int pGrade = int.Parse(userInput);
+        string pLetter = "";
+        if (pGrade >= 90)
+        {
+            pLetter = "A";
+        }
+        else if (pGrade >= 80)
+        {
+            pLetter = "B";
+        }
+        else if (pGrade >= 70)
+        {
+            pLetter = "C";
+        }
+        else if (pGrade >= 60)
+        {
+            pLetter = "D";
+        }
+        else
+        {
+            pLetter = "F";
+        }
+
+        Console.WriteLine($"Your grade in letter is {pLetter}");
+        
+        if (pGrade >=70)
+        {
+            Console.WriteLine("Congratulations! You passed the course.");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, you will acomplish it for sure the next attempt");
+        }
     }
 }
