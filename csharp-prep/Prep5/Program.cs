c5e19e93acff2bfb2a96f662c58b73e05d0c2604
Joel Salvatierra <joeljsalvatierra@gmail.com>
2023-04-18 00:35:17 -0300

    Learning Activity - C# Prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        Welcome();
+        string name = Name();
+        int number = Number();
+        int square = Square(number);
+        Display(name, square);
     }
+        
+
+    static void Welcome()
+    {
+        Console.WriteLine("Welcome to this program");
+    }
+    static string Name()
+    {
+        Console.Write("Enter your name: ");
+        string userName = Console.ReadLine();
+        return userName;
+    }
+
+    static int Number()
+    {
+        Console.Write("Enter your favorite number: ");
+        string userInput = Console.ReadLine();
+        int userNumber = int.Parse(userInput);
+        return userNumber;
+    }
+
+    static int Square(int userNumber)
+    {
+        int square = userNumber * userNumber;
+        return square;
+    }
+
+    static void Display (string name, int square)
+    {
+        Console.Write($"Brother {name}, the square of your number is {square}");
+    }
+
 }
