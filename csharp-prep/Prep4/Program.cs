936b4b056aed9ba3df466277f971d5d1318a248e
Joel Salvatierra <joeljsalvatierra@gmail.com>
2023-04-18 00:12:20 -0300

    Learning Activity - C# Prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        
+        
+        
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        int inputNumber = -1;
+        List<int> numbers = new List<int>();
+        // Add the numbers to the list
+        while (inputNumber != 0)
+        {
+            Console.Write("Enter number: ");
+            string userInput = Console.ReadLine();
+            inputNumber = int.Parse(userInput);
+            if (inputNumber != 0)
+            {
+                numbers.Add(inputNumber);
+            }
+        }
+        
+        // Additioning every number
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum = number + sum;
+        }
+        Console.WriteLine($"The sum is: {sum}");
+
+        // Averaging numbers
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        // Get the largest number
+        int largest = 0;
+        foreach (int number in numbers)
+        {
+            if (number > largest)
+            {
+                largest = number;
+            }
+        }
+        Console.WriteLine($"The largest number is: {largest}");
+
     }
 }
