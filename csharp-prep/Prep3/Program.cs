abaf02abd4fd702f54b9dd6441c5307dc622d475
Joel Salvatierra <joeljsalvatierra@gmail.com>
2023-04-17 23:43:46 -0300

    Learning Activity - C# Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int mNumber = randomGenerator.Next(1, 101);
+        int gNumber = -1;
+        while (mNumber != gNumber)
+        {
+        Console.Write("What is your guess? ");
+        string userInput = Console.ReadLine();
+        gNumber = int.Parse(userInput);
+        if (gNumber > mNumber)
+        {
+            Console.WriteLine("Lower");
+        }
+        else if (gNumber < mNumber)
+        {
+            Console.WriteLine("Higher");
+        }
+        else
+        {
+            Console.WriteLine("You guessed it!");
+        }
+        } 
+        
+
     }
 }
