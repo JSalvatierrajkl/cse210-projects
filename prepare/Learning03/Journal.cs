64036428d6802050af66b26ca3ac0c6c830ab186
Joel Salvatierra <joeljsalvatierra@gmail.com>
2023-05-12 22:17:43 -0300

    Semi-done page

---
+++ prepare/Learning03/Journal.cs
@@ -0,0 +1,24 @@
+public class Journal
+{
+    public string _date;
+    public string _prompt;
+    public string _response;
+
+    public List<Journal> _entries = new List<Journal>();
+
+    //public List<string> _prompts = new List<Journal>();
+    
+    public void Display()
+    {
+
+        foreach (var i in _entries)
+            {
+                i.EntryDisplay();
+            }
+    }
+    
+    public void EntryDisplay()
+    {
+       Console.WriteLine($"{_date} ({_prompt}) {_response}");
+    }
+}
--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,53 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        int inputNumber = -1;
+        List<string> prompts = new List<string>() {"Who was the most interesting person I interacted with today?", 
+        "What was the best part of my day?", 
+        "How did I see the hand of the Lord in my life today?", 
+        "What was the strongest emotion I felt today?", 
+        "If I had one thing I could do over today, what would it be?",
+        "What did I see different today?",
+        "How could you explain the best part of the day in one word", 
+        "Did you see the hand of God today?", 
+        "How did you face the problems?",
+        "What could you do better for tomorrow?"};
+        List<Journal> entry= new List<Journal>();
+        
+        while (inputNumber != 0)
+        {
+            Console.WriteLine("1. Write a new entry");
+            Console.WriteLine("2. Display the journal");
+            Console.WriteLine("3. Save the journal to a file");
+            Console.WriteLine("4. Load the journal from a file");
+            Console.WriteLine("5. Exit");
+            string inputOption = Console.ReadLine();
+            inputNumber = int.Parse(inputOption);
+            
+
+            if (inputNumber == 1)
+            {
+                DateTime theCurrentTime = DateTime.Now;
+                Random rnd = new Random();
+                int randIndex = rnd.Next(prompts.Count);
+                string random = prompts[randIndex];
+                Console.WriteLine(random);
+                Journal entry1 = new Journal();
+                entry1._date = theCurrentTime.ToShortDateString();
+                entry1._prompt = (random);
+                entry1._response = Console.ReadLine();
+                entry1._entries.Add(entry1);
+                entry1.Display();
+                
+            }
+            
+            else if (inputNumber == 2)
+            {
+                Journal myDisplay = new Journal();
+                myDisplay.Display();
+            }
+        }
+
+        
     }
 }
